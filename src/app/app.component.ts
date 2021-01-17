5705a0be6852daeb82360df477a324de1dbb2c51
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-01-17 21:21:23 +0900

    タイムラインを表示する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  {name: "本多 淳", message: "good morning"},
+  {name: "田中 将大", message: "good afternoon"},
+  {name: "大谷 翔平", message: "good evening"},
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+
+  comments = COMMENTS;
 }
