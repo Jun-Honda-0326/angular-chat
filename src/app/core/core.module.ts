a78d249514dc8bd12a13c88cce349860f7c2acb0
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-01-23 10:11:35 +0900

    headerの作成

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firbase),
     AngularFireAuthModule,
     AngularFireDatabaseModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
