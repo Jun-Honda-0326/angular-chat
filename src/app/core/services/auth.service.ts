bc460fb3d4c176d979bfa84d9358df5071264ec8
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-01-24 10:09:46 +0900

    ログイン、ログアウトの実装

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -14,4 +14,13 @@ export class AuthService {
     return this.afAuth.createUserWithEmailAndPassword(email, password);
   }
 
+  login(email: string, password: string): Promise<firebase.auth.UserCredential | void> {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+      .catch(error => console.error(error))
+  }
+
+  logout(): Promise<void> {
+    return this.afAuth.signOut();
+  }
+
 }
