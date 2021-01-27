bbc4a5494f48365d2b92c05b9fa290592abada3f
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-01-28 08:04:13 +0900

    guardの実装

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.afAuth.authState.pipe(
+      map((user: firebase.User) => {
+        if (!user) {
+          return true;
+        } else {
+          return this.router.parseUrl('/')
+        }
+      })
+    )
+  }
+
+}
